53d0488fd3fa22505351c017462214f2e7e96dcf
Yao Bin Then <alvinthen890703@gmail.com>
2016-01-24 13:15:16 +0800

    use patched RN 0.18.0

--- node_modules/react-native/local-cli/generator-android/templates/package/MainActivity.java
+++ node_modules/react-native/local-cli/generator-android/templates/package/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-        new MainReactPackage()
-      );
+        new MainReactPackage());
     }
 }
